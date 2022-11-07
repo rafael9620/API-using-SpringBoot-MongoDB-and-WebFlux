4f89b79a3e56592f3516cb077e106485ca74b760
Francisco R Espinosa <francisco.espinosa@labanca.com.uy>
2022-11-07 16:18:57 -0300

    Primer commit antes de agrefar todas la conf iniciales

---
+++ src/main/java/com/training/reactive/ApiUsingSpringBootMongoDbAndWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.training.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsingSpringBootMongoDbAndWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUsingSpringBootMongoDbAndWebFluxApplication.class, args);
+	}
+
+}
