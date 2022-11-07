4bbc18718eb8bbad6af231f7d19412cf1eab84c6
Francisco R Espinosa <francisco.espinosa@labanca.com.uy>
2022-11-07 17:54:12 -0300

    Creado el controlador para la api

---
+++ src/main/java/com/training/reactive/controllers/ClientController.java
@@ -0,0 +1,9 @@
+package com.training.reactive.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/client")
+public class ClientController {
+}
