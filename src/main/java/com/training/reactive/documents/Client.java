3ec499f32d7e3e2a66ec7f63e91ccffd49a2e7da
Francisco R Espinosa <francisco.espinosa@labanca.com.uy>
2022-11-07 16:42:51 -0300

    Creado el primer document y añadida la dependencia para validar campos

---
+++ src/main/java/com/training/reactive/documents/Client.java
@@ -0,0 +1,29 @@
+package com.training.reactive.documents;
+
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Document
+@Data
+public class Client {
+    @Id
+    private String id;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String lastName;
+    @NotNull
+    private String emial;
+    @NotNull
+    private String age;
+    @NotNull
+    private String salary;
+
+    public Client() {
+    }
+}
